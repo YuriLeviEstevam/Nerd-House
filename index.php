4b3349806ced044a0df73acaeaafaf1af922fa67
Nicolas Simplicio <nicolasanatolio1@gmail.com>
2025-04-03 19:41:17 -0300

    Atualização de Pastas e Index

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <!DOCTYPE html>
 <html>
-<<<<<<< HEAD
-=======
-
->>>>>>> eea61e4bd9166380ae6e0ee595a0ce300e5b9f85
 <head>
   <!-- Basic -->
   <meta charset="utf-8" />
@@ -14,7 +10,6 @@
   <meta name="keywords" content="" />
   <meta name="description" content="" />
   <meta name="author" content="" />
-<<<<<<< HEAD
   <link rel="shortcut icon" href="assets/images/logo-nerd.jpg" type="">
 
   <title>Nerd House</title>
@@ -55,29 +50,6 @@
 
 
 
-=======
-  <link rel="shortcut icon" href="images/logo-nerd.jpg" type="">
-
-  <title>NERD HOUSE</title>
-
-  <!-- bootstrap core css -->
-  <link rel="stylesheet" type="text/css" href="./css/bootstrap.css" />
-
-  <!--owl slider stylesheet -->
-  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
-  <!-- nice select  -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-nice-select/1.1.0/css/nice-select.min.css" integrity="sha512-CruCP+TD3yXzlvvijET8wV5WxxEh5H8P4cmz0RFbKK6FlZ2sYl3AEsKlLPHbniXKSrDdFewhbmBK5skbdsASbQ==" crossorigin="anonymous" />
-  <!-- font awesome style -->
-  <link href="css/font-awesome.min.css" rel="stylesheet" />
-
-  <!-- Custom styles for this template -->
-  <link href="css/style.css" rel="stylesheet" type="text/css" />
-  <!-- responsive style -->
-  <link href="css/responsive.css" rel="stylesheet" />
-
-</head>
-
->>>>>>> eea61e4bd9166380ae6e0ee595a0ce300e5b9f85
 <body class="u">
 
   <div class="hero_area">
@@ -1312,7 +1284,6 @@
         </p>
       </div>
     </div>
-<<<<<<< HEAD
 
   </footer>
 
@@ -1341,32 +1312,4 @@
   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCh39n5U-4IoWpsVGUHWdqB6puEkhRLdmI&callback=myMap"></script>
 
 </body>
-=======
-  </footer>
-  <!-- footer section -->
-
-  <!-- jQery -->
-  <script src="js/jquery-3.4.1.min.js"></script>
-  <!-- popper js -->
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
-  </script>
-  <!-- bootstrap js -->
-  <script src="js/bootstrap.js"></script>
-  <!-- owl slider -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js">
-  </script>
-  <!-- isotope js -->
-  <script src="https://unpkg.com/isotope-layout@3.0.4/dist/isotope.pkgd.min.js"></script>
-  <!-- nice select -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-nice-select/1.1.0/js/jquery.nice-select.min.js"></script>
-  <!-- custom js -->
-  <script src="js/custom.js"></script>
-  <!-- Google Map -->
-  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCh39n5U-4IoWpsVGUHWdqB6puEkhRLdmI&callback=myMap">
-  </script>
-  <!-- End Google Map -->
-
-</body>
-
->>>>>>> eea61e4bd9166380ae6e0ee595a0ce300e5b9f85
 </html>
